f52c7987a439a6e9e78394dde9b89c8b3f8bea74
crazyboycjr <crazyboycjr@gmail.com>
2016-10-07 20:17:28 +0800

    temporary save

--- client.js
+++ client.js
@@ -1,26 +1,124 @@
 'use strict';
 
 const net = require('net');
+const dgram = require('dgram');
+const assert = require('assert');
 
 var HOST = '127.0.0.1';
 var PORT = '12345';
+const TIMEOUT = 1000;
 
-var socket = net.connect({
-	port: PORT,
-	host: HOST
-}, () => {
-	console.log('Connected to server!');
-});
+// num1 and num2 should be String type
+function tcp_send(num1, num2) {
+	num1 = String(num1);
+	num2 = String(num2);
 
-socket.on('connect', () => {
-	socket.write('123  456done\n');
-});
+	let socket = net.connect({
+		port: PORT,
+		host: HOST
+	}, () => {
+		console.log('Connected to server!');
+	});
 
-socket.on('data', (data) => {
-	console.log(data.toString());
-	socket.end();
-});
+	socket.setEncoding('utf8');
 
-socket.on('end', () => {
-	console.log('disconnect from server');
-});
+	socket.on('connect', () => {
+		socket.write(num1);
+		socket.write(' ');
+		socket.write(num2);
+		socket.write('done\n');
+	});
+
+	socket.on('data', (data) => {
+		console.log(data.toString());
+		socket.end();
+	});
+
+	socket.on('end', () => {
+		console.log('disconnect from server');
+	});
+
+	socket.setTimeout(TIMEOUT, () => {
+		console.log('%sms timeout', TIMEOUT);
+		socket.close();
+	});
+
+}
+
+// num1 and num2 should be String type
+function udp_send(num1, num2) {
+	num1 = String(num1);
+	num2 = String(num2);
+
+	var PACKLEN = 512;
+
+	let socket = dgram.createSocket('udp4');
+	socket.on('error', (err) => {
+		console.log(err);
+		socket.close();
+	});
+
+	let send_buffer = new Map();
+	function send_package(type, no, data) {
+
+		if (!send_buffer.has(no)) {
+			let msg_no = Buffer.from(no);
+			let msg_len = Buffer.from(data.length);
+			let msg_text = Buffer.from(data);
+
+			assert(data !== '');
+			send_buffer.set(no, [msg_no, msg_len, msg_text]);
+		}
+
+		socket.send(send_buffer.get(no), PORT, HOST, (err) => {
+			console.log(err);
+			socket.close();	
+		});
+	}
+
+	function send_data(data) {
+
+		send_package(0, String(data.length));
+
+		let pos = 0, no = 1;
+		for (; pos < data.length; pos += PACKLEN, no++) {
+			let len = PACKLEN;
+			if (pos + len > data.length)
+				len = data.length - pos;
+
+			send_package(1, no, data.substr(data, len));
+		}
+	}
+
+	let data = num1 + ' ' + num2;
+	send_data(data);
+
+	let ans = '';
+	
+	socket.on('message', (msg, rinfo) => {
+		msg = Buffer.from(msg);
+		// Check msg TYPE
+		if (msg[0] == 0x1) { // RSD
+			
+			// We need to resend the package
+			let no = msg.readInt32BE(1);
+			console.log('resend no = ', no);
+			send_package(1, no, '');
+
+		} else if (msg[0] == 0x0) { // NOR
+
+			let no = msg.readInt32BE(1);
+			let sz = msg.readInt16BE(5);
+			
+			assert(sz > 0);
+			assert(msg.length === 1 + 4 + 2 + sz);
+
+			ans += msg.toString('utf8', 7, sz);
+
+		} else {
+			console.log('msg TYPE error');
+			assert(0);
+		}
+	});
+
+}
--- server.js
+++ server.js
@@ -1,48 +1,116 @@
 'use strict';
 const net = require('net');
+const dgram = require('dgram');
+const util = require('util');
+const assert = require('assert');
 
 const HOST = '0.0.0.0';
 const PORT = 12345;
-const BACKLOG = 511; // Default value of system
+const BACKLOG = 511; // Default value of node 
+const TIMEOUT = 1000;
 
-var server = net.createServer();
-server.listen({
-	port: PORT,
-	host: HOST,
-	backlog: BACKLOG
-}, () => { console.log(server.address()); });
+function start_tcp_server() {
 
-server.on('connection', function(socket) {
+	let server = net.createServer();
+	server.listen({
+		port: PORT,
+		host: HOST,
+		backlog: BACKLOG
+	}, () => {
+		console.log('TCP:', server.address());
+	});
 
-	console.log('Connected: ', socket.remoteAddress + ':' + socket.remotePort);
+	server.on('connection', function(socket) {
 
-	socket.on('end', () => { console.log(`client ${socket.remoteAddress}:${socket.remotePort} disconnected`); });
-	
-	let data = '';
-	socket.on('data', (chunk) => {
-		data += chunk;
-		if (data.endsWith('done\n') || data.endsWith('done')) {
-			// 这里以done结尾作为结束
-			data = data.trim();
-			if (data.endsWith('done'))
-				data = data.substring(0, data.length - 4);
-			data = data.trim();
-			data = data.replace(/ +/g, ' ');
+		console.log('Connected: ', socket.remoteAddress + ':' + socket.remotePort);
+		
+		socket.setEncoding('utf8');
+		socket.on('end', () => {
+			console.log(`client ${socket.remoteAddress}:${socket.remotePort} disconnected`);
+		});
 
-			let [num1, num2] = data.split(' ');
-			console.log(num1, num2);
-			let ans = add(num1, num2);
-			console.log(ans);
-			socket.write(ans);
-			socket.end();
+		let data = '';
+		socket.on('data', (chunk) => {
+			data += chunk;
+			if (data.endsWith('done\n') || data.endsWith('done')) {
+				// 在TCP传输中我们规定以done结尾作为结束
+				data = data.trim();
+				if (data.endsWith('done'))
+					data = data.substring(0, data.length - 4);
+				data = data.trim();
+				data = data.replace(/ +/g, ' ');
+
+				let [num1, num2] = data.split(' ');
+				console.log(num1, num2);
+
+				let ans = add(num1, num2);
+				console.log(ans);
+
+				socket.write(ans);
+				socket.end();
+			}
+		});
+
+	});
+
+	server.on('error', (err) => {
+		console.log(err);
+		setTimeout(() => {
+			server.close();
+			server.listen({
+				port: PORT,
+				host: HOST,
+				backlog: BACKLOG
+			}, TIMEOUT);
+		});
+	});
+}
+
+start_tcp_server();
+
+
+function start_udp_server() {
+
+	let socket = dgram.createSocket('udp4');
+
+	socket.bind(PORT);
+	socket.on('listening', () => {
+		let addr = socket.address();
+		console.log('UDP:', socket.address());
+	});
+
+	socket.on('error', (err) => {
+		console.log(err);
+	});
+
+	let user_pool = new Map();
+	socket.on('message', (msg, rinfo) => {
+		console.log('Received %d bytes from %s:%d\n',
+			msg.length, rinfo.address, rinfo.port);
+
+		let key = util.format('%s:%s', rinfo.address, rinfo.port);
+
+		//socket.send(util.inspect(udpMap.get(key)), rinfo.port, rinfo.address);
+		
+		if (msg[0] == 0x0) {
+
+		} else if (msg[0] == 0x1) {
+		} else {
+			console.log('msg type error');
+			assert(0);
 		}
 	});
 
-});
+}
 
-server.on('error', (err) => {
-	console.log(err);
-})
+start_udp_server();
+
+
+
+
+
+
+
 
 
 function add(a, b) {
